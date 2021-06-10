cd1157a1575a170c1ca0ba38b53a54ea03d28544
zx <3313147896@qq.com>
2021-06-10 21:55:33 +0800

    feat: AppHeader中选项卡的完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,12 +1,50 @@
 import React, { memo } from "react";
+
 import { NavLink } from "react-router-dom";
+import { headerLinks } from "@/services/local-data";
+import { AppHeaderWrapper, HeaderLeft, HeaderRight } from "./style";
 
 export default memo(function ZXAppHeader() {
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink exact to={item.link}>
+          {item.title}
+          <i className={"sprite_01 icon"}></i>
+        </NavLink>
+      );
+    } else {
+      return (
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
+          {item.title}
+        </a>
+      );
+    }
+  };
+
   return (
-    <div>
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">朋友</NavLink>
-    </div>
+    <AppHeaderWrapper>
+      <div className="wrap-v1 content">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="#/">
+            网易云音乐
+          </a>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div className={"select-item"} key={item.title}>
+                  {showSelectItem(item, index)}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          {/* <div className="center">创作者中心</div>
+          <div className="">登录</div> */}
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </AppHeaderWrapper>
   );
 });
