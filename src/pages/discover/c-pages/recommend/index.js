797eeb637271da42409728d2749c8041d1d42cf5
zx <3313147896@qq.com>
2021-06-17 15:55:05 +0800

    feat: 集成immutableJs

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,13 +1,16 @@
 import React, { memo, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 import { getTopBannersAction } from "./store/actionCreators";
 
 function ZXRecommend() {
   // 组件和redux关联：获取数据和进行操作
+  // useSelector 默认为 === 比较
   const { topBanners } = useSelector((state) => ({
-    topBanners: state.recommend.topBanners,
-  }));
+    // topBanners: state.get("recommend").get("topBanners"),
+    topBanners: state.getIn(["recommend", "topBanners"]),
+    // shallowEqual 进行浅层比较
+  }), shallowEqual);
   const dispatch = useDispatch();
   // 发送网络请求
   useEffect(() => {
--- src/pages/discover/c-pages/recommend/store/reducer.js
+++ src/pages/discover/c-pages/recommend/store/reducer.js
@@ -1,15 +1,17 @@
+import { Map } from "immutable";
+
 import * as actionTypes from "./constants";
-const defaultState = {
+const defaultState = Map({
   topBanners: [],
-};
+});
 
 function reducer(state = defaultState, action) {
   switch (action.type) {
     case actionTypes.CHANGE_TOP_BNNAERS:
-      return { ...state, topBanners: action.topBanners };
+      return state.set("topBanners", action.topBanners);
     default:
       return state;
   }
 }
 
-export default reducer
+export default reducer;
