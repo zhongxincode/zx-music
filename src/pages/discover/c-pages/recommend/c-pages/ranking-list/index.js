3b9fbe7de3bd6e7680f2e013b816b5af48906bc4
zx <3313147896@qq.com>
2021-06-19 15:37:55 +0800

    feat: recommend榜单组件的完成

--- src/pages/discover/c-pages/recommend/c-pages/ranking-list/index.js
+++ src/pages/discover/c-pages/recommend/c-pages/ranking-list/index.js
@@ -1,12 +1,21 @@
 import React, { memo, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 import ZXThemeHeaderRCM from "@/components/theme-header-rcm";
+import ZXTopRanking from "@/components/top-ranking";
 
 import { RankingWrapper } from "./style";
 import { getTopListAction } from "../../store/actionCreators";
 export default memo(function ZXRankingList() {
   // redux hooks
+  const { upRanking, newRanking, originRanking } = useSelector(
+    (state) => ({
+      upRanking: state.getIn(["recommend", "upRanking"]),
+      newRanking: state.getIn(["recommend", "newRanking"]),
+      originRanking: state.getIn(["recommend", "originRanking"]),
+    }),
+    shallowEqual
+  );
   const dispatch = useDispatch();
   // hooks
   useEffect(() => {
@@ -17,6 +26,11 @@ export default memo(function ZXRankingList() {
   return (
     <RankingWrapper>
       <ZXThemeHeaderRCM title={"榜单"} />
+      <div className="tops">
+        <ZXTopRanking info={upRanking} />
+        <ZXTopRanking info={newRanking} />
+        <ZXTopRanking info={originRanking} />
+      </div>
     </RankingWrapper>
   );
 });
