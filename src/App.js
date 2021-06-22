9771c97e0f00cf24d8e145062bd250fe400c53a2
zx <3313147896@qq.com>
2021-06-22 11:01:02 +0800

    feat: 使用懒加载加载组件

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, Suspense } from "react";
 import { Provider } from "react-redux";
 import { renderRoutes } from "react-router-config";
 import { HashRouter } from "react-router-dom";
@@ -8,14 +8,16 @@ import store from "./store";
 
 import ZXAppHeader from "@/components/app-header";
 import ZXAppFooter from "@/components/app-footer";
-import ZXAppPlayerBar from '@/pages/player/app-player-bar'
+import ZXAppPlayerBar from "@/pages/player/app-player-bar";
 
 export default memo(function App() {
   return (
     <Provider store={store}>
       <HashRouter>
         <ZXAppHeader />
-        {renderRoutes(routes)}
+        <Suspense fallback={<div>page loading</div>}>
+          {renderRoutes(routes)}
+        </Suspense>
         <ZXAppFooter />
         <ZXAppPlayerBar />
       </HashRouter>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
+import React from 'react'
 import { Redirect } from "react-router";
 
-import ZXDiscover from "@/pages/discover";
-import ZXMine from "@/pages/mine";
-import ZXFriend from "@/pages/friend";
-import ZXRecommend from "@/pages/discover/c-pages/recommend";
-import ZXAlbum from "@/pages/discover/c-pages/album";
-import ZXArtist from "@/pages/discover/c-pages/artist";
-import ZXDjradio from "@/pages/discover/c-pages/djradio";
-import ZXToplist from "@/pages/discover/c-pages/toplist";
-import ZXPlaylist from "@/pages/discover/c-pages/playlist";
-import ZXPlayer from "@/pages/player";
+const ZXDiscover = React.lazy(() => import("@/pages/discover"));
+const ZXMine = React.lazy(() => import("@/pages/mine"));
+const ZXFriend = React.lazy(() => import("@/pages/friend"));
+const ZXRecommend = React.lazy(() => import("@/pages/discover/c-pages/recommend"));
+const ZXAlbum = React.lazy(() => import("@/pages/discover/c-pages/album"));
+const ZXArtist = React.lazy(() => import("@/pages/discover/c-pages/artist"));
+const ZXDjradio = React.lazy(() => import("@/pages/discover/c-pages/djradio"));
+const ZXToplist = React.lazy(() => import("@/pages/discover/c-pages/toplist"));
+const ZXPlaylist = React.lazy(() => import("@/pages/discover/c-pages/playlist"));
+const ZXPlayer = React.lazy(() => import("@/pages/player"));
 
 const routes = [
   {
